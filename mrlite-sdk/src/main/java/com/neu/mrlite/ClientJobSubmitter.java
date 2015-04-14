05d9ad0e6662dc8005a962082b5674270fea9547
PramodKhare <kharepr@ccs.neu.edu>
2015-04-14 02:58:50 -0400

    Modified the way Client job gets submitted and Modified the test program with reducer function

--- mrlite-sdk/src/main/java/com/neu/mrlite/ClientJobSubmitter.java
+++ mrlite-sdk/src/main/java/com/neu/mrlite/ClientJobSubmitter.java
@@ -1,40 +1,64 @@
 package com.neu.mrlite;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.lang.reflect.Array;
-import java.lang.reflect.Type;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.Socket;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.Locale;
 
 public class ClientJobSubmitter {
-	public static void main(String args[]) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, ClassNotFoundException, MalformedURLException {
-		try {
-			if(args.length < 5)
-				usage();
-			Socket socket = new Socket(args[0], 2121);
-		    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-		    BufferedReader in = new BufferedReader( new InputStreamReader(socket.getInputStream()));
-	    	out.println(args[1]+" "+args[2]+" "+args[3]+" "+args[4]);
-			String line;
-			while((line = in.readLine()) != null) {
-				System.out.println(line);
-			}
-			socket.close();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void usage() {
-		System.out.println("usage: java -jar maelite-sdk-*-SNAPSHOT.jar <MasterNodeIP> <JobJarLocation> <JobClass> <inFile> <outDir>");
-		System.exit(1);
-	}
+    public static void main(String args[]) throws InstantiationException,
+            IllegalAccessException, IllegalArgumentException,
+            InvocationTargetException, NoSuchMethodException,
+            SecurityException, ClassNotFoundException, MalformedURLException {
+
+        Socket socket = null;
+        PrintWriter out = null;
+        BufferedReader in = null;
+
+        try {
+            // Check if user has submitted the required minimum number of arguments
+            if (args.length < 5) {
+                usage();
+            }
+
+            socket = new Socket(args[0], 2121);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(
+                    socket.getInputStream()));
+
+            // Take all remaining arguments and send them to JobListener
+            StringBuffer jobCommand = new StringBuffer();
+            for (int i = 1; i < args.length; i++) {
+                jobCommand.append(args[i]).append(" ");
+            }
+            out.println(jobCommand.toString());
+            String line;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+                if (out != null) {
+                    out.close();
+                }
+                socket.close();
+            } catch (final Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void usage() {
+        // Command to be submitted to Job Listener => mrlite -jar <jar_name> <main_class> <input_file_path> <output_directory_path>
+        System.out
+                .println("usage: ClientJobSubmitter <JobListenerHost> mrlite -jar <jar_name> <main_class> <input_file_path> <output_directory_path>");
+        System.exit(1);
+    }
 }
